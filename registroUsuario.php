54124ed2d2eb1d1abc52d4403a50bc1d9c83c292
Lautaro Luna <128639299+lautaro-luna@users.noreply.github.com>
2024-08-17 13:51:36 -0300

    registro usuario completado

--- registroUsuario.php
+++ registroUsuario.php
@@ -4,17 +4,24 @@
 if ($_POST) {
 
     // Extraer de la bd una lista con todos los usuarios
-    $sentenciaSQL = $conexion->prepare("SELECT * FROM Usuarios");
+    $sentenciaSQL = $conexion->prepare("SELECT * FROM Usuarios WHERE Email_Usuario LIKE :email");
+    $sentenciaSQL->bindParam(':email', $_POST['txtEmail']);
     $sentenciaSQL->execute();
     $listaUsuarios = $sentenciaSQL->fetchAll(PDO::FETCH_ASSOC);
+
+    $emailResgistrado = false;
 
     // Recorrer arreglo de usuarios
     foreach ($listaUsuarios as $usuario) {
         // Revisar si el usuario está registrado
-        if ($_POST['txtEmail'] == $usuario['Email_Usuario']) {
+        if (isset($usuario['Email_Usuario'])) {
             $mensaje = "Este correo ya está registrado.";
-        } else {
-            //Recibir los datos del formulario y guardarlo en variables. Si no hay datos se guardan vacías
+            $emailResgistrado = true;
+        }
+    }
+
+    if (!$emailResgistrado) {
+        //Recibir los datos del formulario y guardarlo en variables. Si no hay datos se guardan vacías
         $txtUsername = (isset($_POST['txtUsername'])) ? $_POST['txtUsername'] : "";
         $txtNombreUsuario = (isset($_POST['txtNombre'])) ? $_POST['txtNombre'] : "";
         $txtApellidosUsuario = (isset($_POST['txtApellidos'])) ? $_POST['txtApellidos'] : "";
@@ -26,33 +33,41 @@ if ($_POST) {
         $txtIdProvincia = (isset($_POST['txtProvincia'])) ? $_POST['txtProvincia'] : "";
         $txtLocalidad = (isset($_POST['txtLocalidad'])) ? $_POST['txtLocalidad'] : "";
 
-        echo $txtIdProvincia;
 
-        // Extraer de la bd una lista con todos los usuarios
+        // Extraer sos
         $sentenciaSQL = $conexion->prepare("SELECT * FROM Localidades WHERE Provincias_ID_Provincia=:IdProvincia AND Nombre_Localidad LIKE :nombreLocalidad ");
         $sentenciaSQL->bindParam(':IdProvincia', $txtIdProvincia);
         $sentenciaSQL->bindParam(':nombreLocalidad', $txtLocalidad);
         $sentenciaSQL->execute();
         $listaLocalidades = $sentenciaSQL->fetchAll(PDO::FETCH_ASSOC);
 
-        foreach($listaLocalidades as $localidad){
-            if (isset($localidad['Nombre_Localidad'])){
-                $txtIdLocalidad = $localidad['ID_Localidades'];
-            } else {
-                $sentenciaSQL = $conexion->prepare("INSERT INTO Localidades (Nombre_Localidad, Provincias_ID_Provincia) VALUES (:NombreLocalidad, :IdProvincia); ");
-                $sentenciaSQL->bindParam(':IdProvincia', $txtIdProvincia);
-                $sentenciaSQL->bindParam(':NombreLocalidad', $txtLocalidad);
-                $sentenciaSQL->execute();
+        $seEncontroLocalidad = false;
 
-                $txtIdLocalidad = $localidad['ID_Localidad'];
+        foreach ($listaLocalidades as $localidad) {
+            if (isset($localidad['Nombre_Localidad'])) {
+                $txtIdLocalidad = $localidad['ID_Localidades'];
+                $seEncontroLocalidad = true;
             }
+        }
+
+        if (!$seEncontroLocalidad) {
+            $sentenciaSQL = $conexion->prepare("INSERT INTO Localidades (Nombre_Localidad, Provincias_ID_Provincia) VALUES (:NombreLocalidad, :IdProvincia); ");
+            $sentenciaSQL->bindParam(':NombreLocalidad', $txtLocalidad);
+            $sentenciaSQL->bindParam(':IdProvincia', $txtIdProvincia);
+            $sentenciaSQL->execute();
+
+            $sentenciaSQL = $conexion->prepare("SELECT ID_Localidades FROM Localidades WHERE Nombre_Localidad=:NombreLocalidad");
+            $sentenciaSQL->bindParam(':NombreLocalidad', $txtLocalidad);
+            $sentenciaSQL->execute();
+            $localidad = $sentenciaSQL->fetch(PDO::FETCH_LAZY);
+            $txtIdLocalidad =  $localidad['ID_Localidades'];
         }
 
 
         // Insertar datos a tabla Usuarios
         $sentenciaSQL = $conexion->prepare("INSERT INTO Usuarios (Username_Usuario, Nombre_Usuario, Apellidos_Usuario, 
-        DNI_Usuario, Email_Usuario, Contrasenia_Usuario, Direccion_Usuario, Telefono_Usuario, Tipos_de_Usuario_ID_Tipos_de_Usuario, Localidades_ID_Localidades) 
-        VALUES (:username, :nombre_usuario, :apellidos_usuario, :dni, :email, :contrasenia, :direccion, :telefono, :tipo_usuario, :id_localidad);");
+    DNI_Usuario, Email_Usuario, Contrasenia_Usuario, Direccion_Usuario, Telefono_Usuario, Tipos_de_Usuario_ID_Tipos_de_Usuario, Localidades_ID_Localidades) 
+    VALUES (:username, :nombre_usuario, :apellidos_usuario, :dni, :email, :contrasenia, :direccion, :telefono, :tipo_usuario, :id_localidad);");
         // Ejecutar la consulta con los valores proporcionados
         $sentenciaSQL->execute([
             ':username' => $txtUsername,
@@ -69,9 +84,6 @@ if ($_POST) {
 
 
         header('Location:loginUsuario.php');
-        }
-        
-
     }
 }
 
@@ -92,38 +104,38 @@ if ($_POST) {
                     <h3 class="fw-normal " style="font-size:small">Informacion Personal</h3>
                     <hr>
                     <div data-mdb-input-init class="form-outline mb-2">
-                        <label class="form-label" style="font-size:small;" >Nombre</label>
-                        <input type="text" name="txtNombre"  class="form-control form-control-md" />
+                        <label class="form-label" style="font-size:small;">Nombre</label>
+                        <input type="text" name="txtNombre" required class="form-control form-control-md" />
                     </div>
 
                     <div data-mdb-input-init class="form-outline mb-2">
-                        <label class="form-label" style="font-size:small;" >Apellidos</label>
-                        <input type="text" name="txtApellidos"  class="form-control form-control-md" />
+                        <label class="form-label" style="font-size:small;">Apellidos</label>
+                        <input type="text" name="txtApellidos" required class="form-control form-control-md" />
                     </div>
 
                     <div id="div-dni-telefono">
                         <div data-mdb-input-init class="DNI mb-2">
-                            <label class="form-label" style="font-size:small;" >DNI</label>
-                            <input type="text" name="txtDni"  class="form-control" />
+                            <label class="form-label" style="font-size:small;">DNI</label>
+                            <input type="text" name="txtDni" required class="form-control" />
                         </div>
 
                         <div data-mdb-input-init class="Telefono mb-2">
-                            <label class="form-label" style="font-size:small;" >Telefono</label>
-                            <input type="text" name="txtTelefono"  class="form-control" />
+                            <label class="form-label" style="font-size:small;">Telefono</label>
+                            <input type="text" name="txtTelefono" required class="form-control" />
                         </div>
                     </div>
 
 
                     <div data-mdb-input-init class="form-outline mb-2">
-                        <label class="form-label" style="font-size:small;" >Dirección</label>
-                        <input type="text" name="txtDireccion"  class="form-control form-control-md" />
+                        <label class="form-label" style="font-size:small;">Dirección</label>
+                        <input type="text" name="txtDireccion" required class="form-control form-control-md" />
                     </div>
 
 
                     <div id="div-provincia-localidad">
                         <div data-mdb-input-init class="Provincia mb-4">
-                            <label class="form-label" style="font-size:small;" >Provincia</label>
-                            <select name="txtProvincia" id="provincia" class="form-control">
+                            <label class="form-label" style="font-size:small;">Provincia</label>
+                            <select name="txtProvincia" id="provincia" class="form-control" required>
                                 <?php
                                 $sentenciaSQL = $conexion->prepare("SELECT * FROM Provincias");
                                 $sentenciaSQL->execute();
@@ -136,8 +148,8 @@ if ($_POST) {
                             </select>
                         </div>
                         <div data-mdb-input-init class="Localidad mb-4">
-                            <label class="form-label" style="font-size:small;" >Localidad</label>
-                            <input type="text" name="txtLocalidad" class="form-control" />
+                            <label class="form-label" style="font-size:small;">Localidad</label>
+                            <input type="text" name="txtLocalidad" required class="form-control" />
                         </div>
                     </div>
 
@@ -146,18 +158,18 @@ if ($_POST) {
                     <h3 class="fw-normal " style="font-size:small;">Informacion de Inicio de Sesion</h3>
                     <hr>
                     <div data-mdb-input-init class="form-outline mb-2">
-                        <label class="form-label" style="font-size:small;" >Nombre de Usuario</label>
-                        <input type="text" name="txtUsername"  class="form-control form-control-md" />
+                        <label class="form-label" style="font-size:small;">Nombre de Usuario</label>
+                        <input type="text" name="txtUsername" required class="form-control form-control-md" />
                     </div>
 
                     <div data-mdb-input-init class="form-outline mb-2">
-                        <label class="form-label" style="font-size:small;" >Correo electrónico</label>
-                        <input type="email" name="txtEmail"  class="form-control form-control-md" />
+                        <label class="form-label" style="font-size:small;">Correo electrónico</label>
+                        <input type="email" name="txtEmail" required class="form-control form-control-md" />
                     </div>
 
                     <div data-mdb-input-init class="form-outline mb-4">
-                        <label class="form-label" style="font-size:small;" >Contraseña</label>
-                        <input type="password" name="txtContrasenia"  class="form-control form-control-md" />
+                        <label class="form-label" style="font-size:small;">Contraseña</label>
+                        <input type="password" name="txtContrasenia" required class="form-control form-control-md" />
                     </div>
 
                     <div id="btn-registro">
