5aa3879a7eb0af2580e50aaf1cbf206ff5d4380d
Lautaro Luna <128639299+lautaro-luna@users.noreply.github.com>
2024-08-18 00:34:11 -0300

    filtro precios

--- productos.php
+++ productos.php
@@ -8,14 +8,38 @@
 
 // Obtener la categoría seleccionada del formulario
 $categoriaSeleccionada = isset($_POST['txtCategoria']) ? $_POST['txtCategoria'] : 'todas';
+// Obtener el filtro seleccionada del formulario
+$filtroSeleccionado = isset($_POST['txtFiltro']) ? $_POST['txtFiltro'] : 'ninguno';
+
 
 if (isset($categoriaSeleccionada)) {
     if ($categoriaSeleccionada == 'todas') {
-        $sentenciaSQL = $conexion->prepare("SELECT * FROM Productos");
+        switch ($filtroSeleccionado) {
+            case 'precioMasBajo':
+                $sentenciaSQL = $conexion->prepare("SELECT * FROM Productos ORDER BY Precio_Producto ASC");
+                break;
+            case 'precioMasAlto':
+                $sentenciaSQL = $conexion->prepare("SELECT * FROM Productos ORDER BY Precio_Producto DESC");
+                break;
+            case 'ninguno':
+                $sentenciaSQL = $conexion->prepare("SELECT * FROM Productos");
+                break;
+        }
         $sentenciaSQL->execute();
         $listaProductos = $sentenciaSQL->fetchAll(PDO::FETCH_ASSOC);
     } else {
-        $sentenciaSQL = $conexion->prepare("SELECT * FROM Productos WHERE Categorias_ID_Categoria=:IdCategoria");
+        switch ($filtroSeleccionado) {
+            case 'precioMasBajo':
+                $sentenciaSQL = $conexion->prepare("SELECT * FROM Productos WHERE Categorias_ID_Categoria=:IdCategoria ORDER BY Precio_Producto ASC");
+                break;
+            case 'precioMasAlto':
+                $sentenciaSQL = $conexion->prepare("SELECT * FROM Productos WHERE Categorias_ID_Categoria=:IdCategoria ORDER BY Precio_Producto DESC");
+                break;
+            case 'ninguno':
+                $sentenciaSQL = $conexion->prepare("SELECT * FROM Productos WHERE Categorias_ID_Categoria=:IdCategoria");
+                break;
+        }
+
         $sentenciaSQL->bindParam(":IdCategoria", $categoriaSeleccionada);
         $sentenciaSQL->execute();
         $listaProductos = $sentenciaSQL->fetchAll(PDO::FETCH_ASSOC);
@@ -31,56 +55,55 @@ if (isset($categoriaSeleccionada)) {
     }
 }
 
+
 ?>
+
+
 
 <br>
 
 <div class="row">
     <!-- Sidebar -->
-    <aside class="col-md-2">
-        
-        <form method="POST" action="">
-            <div data-mdb-input-init class="categoria mb-2">
-                <label class="form-label">Filtrar por:</label>
-                <select name="txtFiltro" id="categoria" class="form-control" onchange="this.form.submit()">
-                    <option value="todas" <?php if ($categoriaSeleccionada == 'todas') echo 'selected'; ?>>Todas las Categorías</option>
-                    <?php
-                    $sentenciaSQL = $conexion->prepare("SELECT * FROM Categorias");
-                    $sentenciaSQL->execute();
-                    $listaCategorias = $sentenciaSQL->fetchAll(PDO::FETCH_ASSOC);
-                    foreach ($listaCategorias as $categoria) { ?>
-                        <option value="<?php echo htmlspecialchars($categoria['ID_Categoria']); ?>" <?php if ($categoriaSeleccionada == $categoria['ID_Categoria']) echo 'selected'; ?>>
-                            <?php echo htmlspecialchars($categoria['Nombre_Categoria']); ?>
-                        </option>
-                    <?php } ?>
-                </select>
-            </div>
-        </form>
-        <div class="card">
-            <div class="card-header">
-                Categorias
-            </div>
-            <div>
-                <form method="POST">
-                    <!-- Botón para Todas las categorías -->
-                    <button type="submit" class="btn text-secondary" style="width: 100%; text-align:left;" name="txtCategoria" value="todas" <?php if ($categoriaSeleccionada == 'todas') echo 'style="font-weight: bold;"'; ?>>
-                        Todas las categorías
-                    </button>
+<aside class="col-md-2">
 
-                    <?php
-                    $sentenciaSQL = $conexion->prepare("SELECT * FROM Categorias");
-                    $sentenciaSQL->execute();
-                    $listaCategorias = $sentenciaSQL->fetchAll(PDO::FETCH_ASSOC);
-                    foreach ($listaCategorias as $categoria) { ?>
-                        <button style="width: 100%; text-align:left;" type="submit" class="btn text-secondary" name="txtCategoria" value="<?php echo htmlspecialchars($categoria['ID_Categoria']); ?>" <?php if ($categoriaSeleccionada == $categoria['ID_Categoria']) echo 'style="font-weight: bold;"'; ?>>
-                            <?php echo htmlspecialchars($categoria['Nombre_Categoria']); ?>
-                        </button>
-                    <?php } ?>
-                </form>
-            </div>
-        </div>
+<form method="POST" action="">
+    <div data-mdb-input-init class="categoria mb-2">
+        <label class="form-label">Filtrar por:</label>
+        <select name="txtFiltro" id="filtro" class="form-control" onchange="this.form.submit()">
+            <option value="ninguno" <?php if ($filtroSeleccionado == 'ninguno') echo 'selected'; ?>>Sin filtro</option>
+            <option value="precioMasBajo" <?php if ($filtroSeleccionado == 'precioMasBajo') echo 'selected'; ?>>Precio más bajo</option>
+            <option value="precioMasAlto" <?php if ($filtroSeleccionado == 'precioMasAlto') echo 'selected'; ?>>Precio más alto</option>
+        </select>
+        <!-- Campo oculto para mantener la categoría seleccionada -->
+        <input type="hidden" name="txtCategoria" value="<?php echo htmlspecialchars($categoriaSeleccionada); ?>">
+    </div>
+</form>
 
-    </aside>
+<div class="card">
+    <div class="card-header">
+        Categorías
+    </div>
+    <div>
+    <form method="POST">
+    <!-- Botón para Todas las categorías -->
+    <button type="submit" class="btn text-secondary" style="width: 100%; text-align:left; <?php if ($categoriaSeleccionada == 'todas') echo 'font-weight: bold;'; ?>" name="txtCategoria" value="todas">
+        Todas las categorías
+    </button>
+
+    <?php
+    $sentenciaSQL = $conexion->prepare("SELECT * FROM Categorias");
+    $sentenciaSQL->execute();
+    $listaCategorias = $sentenciaSQL->fetchAll(PDO::FETCH_ASSOC);
+    foreach ($listaCategorias as $categoria) { ?>
+        <button style="width: 100%; text-align:left; <?php if ($categoriaSeleccionada == $categoria['ID_Categoria']) echo 'font-weight: bold;'; ?>" type="submit" class="btn text-secondary" name="txtCategoria" value="<?php echo htmlspecialchars($categoria['ID_Categoria']); ?>">
+            <?php echo htmlspecialchars($categoria['Nombre_Categoria']); ?>
+        </button>
+    <?php } ?>
+</form>
+    </div>
+</div>
+
+</aside>
 
     <!-- Productos -->
     <div class="col-md-10">
@@ -91,10 +114,10 @@ if (isset($categoriaSeleccionada)) {
                         <img class="card-img-top" src="./imgProductos/<?php echo htmlspecialchars($producto['Imagen_Producto']) ?>" alt="">
                         <div class="card-body">
                             <h5 class="card-title"><?php echo htmlspecialchars($producto['Nombre_Producto']) ?></h5>
-                                <p class="text-info"><?php echo "$ ".htmlspecialchars($producto['Precio_Producto']) ?></p>
-                                <?php if($producto['Tiene_Stock_Producto']==0) { ?>
-                                    <p class="text-danger"><?php echo "Sin Stock" ?></p>
-                                <?php } ?>
+                            <p class="text-info"><?php echo "$ " . htmlspecialchars($producto['Precio_Producto']) ?></p>
+                            <?php if ($producto['Tiene_Stock_Producto'] == 0) { ?>
+                                <p class="text-danger"><?php echo "Sin Stock" ?></p>
+                            <?php } ?>
                         </div>
                     </div>
                 </div>
