b74b29567bd8c90d5a957719d172d6fba378b9c6
lautaro-luna <128639299+lautaro-luna@users.noreply.github.com>
2024-08-21 11:20:14 -0300

    "Refactored product listing code to use a loop for categorization, removed duplicate code, and added semicolons to include statements."

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php include("template/cabecera.php") ?>
+<?php include("template/cabecera.php"); ?>
 
 <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel" style="z-index: -999; margin-top: 0.5%">
   <div class="carousel-inner">
@@ -21,160 +21,78 @@
   <?php
   include("administrador/config/bd.php");
 
-    $sentenciaSQL = $conexion->prepare("SELECT * FROM Productos ORDER BY ID_Producto DESC LIMIT 10");
+  $sentenciaSQL = $conexion->prepare("SELECT * FROM Productos ORDER BY ID_Producto DESC LIMIT 10");
+  $sentenciaSQL->execute();
+  $listaProductos = $sentenciaSQL->fetchAll(PDO::FETCH_ASSOC);
+
+  ?>
+  <div class="">
+    <div class="row d-flex">
+      <?php foreach ($listaProductos as $producto) {
+        $formId = 'postForm' . htmlspecialchars($producto['ID_Producto']);
+      ?>
+        <div class="">
+          <form id="<?php echo $formId; ?>" action="productoDetalle.php" method="GET">
+            <input type="hidden" name="IdProducto" value="<?php echo htmlspecialchars($producto['ID_Producto']) ?>">
+            <a href="#" onclick="document.getElementById('<?php echo $formId; ?>').submit();">
+              <div class="cardLista">
+                <div class="cardProdInicio">
+                  <img class="card-img-topProd img-square" src="./imgProductos/<?php echo htmlspecialchars($producto['Imagen_Producto']) ?>" alt="">
+                  <div class="card-bodyProd">
+                    <h5 class="card-titleProd"><?php echo htmlspecialchars($producto['Nombre_Producto']) ?></h5>
+                    <p class="text-infoProd"><?php echo "$ " . htmlspecialchars($producto['Precio_Producto']) ?></p>
+                    <?php if ($producto['Tiene_Stock_Producto'] == 0) { ?>
+                      <p class="text-danger stock-labelProd"><?php echo "Sin Stock" ?></p>
+                    <?php } ?>
+                  </div>
+                </div>
+              </div>
+
+            </a>
+          </form>
+        </div>
+      <?php } ?>
+    </div>
+  </div>
+
+  <?php $arrayCategorias = ['Futbol', 'Caza y Pesca', 'Rugby'];
+  foreach ($arrayCategorias as $categoria) { ?>
+    <br>
+    <h3> <?php echo htmlspecialchars($categoria)?> </h3>
+    <hr>
+    <?php
+    $sentenciaSQL = $conexion->prepare("SELECT * FROM Productos INNER JOIN Categorias ON Productos.Categorias_ID_Categoria=Categorias.ID_Categoria WHERE Categorias.Nombre_Categoria=:categoria ORDER BY Productos.ID_Producto DESC LIMIT 10");
+    $sentenciaSQL->bindParam(":categoria", $categoria);
     $sentenciaSQL->execute();
-    $listaProductos = $sentenciaSQL->fetchAll(PDO::FETCH_ASSOC);
-  
-  ?>
-  <div class="">
-    <div class="row d-flex">
-      <?php foreach ($listaProductos as $producto) {
-        $formId = 'postForm' . htmlspecialchars($producto['ID_Producto']);
-      ?>
-        <div class="">
-          <form id="<?php echo $formId; ?>" action="productoDetalle.php" method="GET">
-            <input type="hidden" name="IdProducto" value="<?php echo htmlspecialchars($producto['ID_Producto']) ?>">
-            <a href="#" onclick="document.getElementById('<?php echo $formId; ?>').submit();">
-              <div class="cardLista">
-                <div class="cardProdInicio">
-                  <img class="card-img-topProd img-square" src="./imgProductos/<?php echo htmlspecialchars($producto['Imagen_Producto']) ?>" alt="">
-                  <div class="card-bodyProd">
-                    <h5 class="card-titleProd"><?php echo htmlspecialchars($producto['Nombre_Producto']) ?></h5>
-                    <p class="text-infoProd"><?php echo "$ " . htmlspecialchars($producto['Precio_Producto']) ?></p>
-                    <?php if ($producto['Tiene_Stock_Producto'] == 0) { ?>
-                      <p class="text-danger stock-labelProd"><?php echo "Sin Stock" ?></p>
-                    <?php } ?>
+    $listaProductos = $sentenciaSQL->fetchAll(PDO::FETCH_ASSOC); ?>
+    <div class="">
+      <div class="row d-flex">
+        <?php foreach ($listaProductos as $producto) {
+          $formId = 'postForm' . htmlspecialchars($producto['ID_Producto']);
+        ?>
+          <div class="">
+            <form id="<?php echo $formId; ?>" action="productoDetalle.php" method="GET">
+              <input type="hidden" name="IdProducto" value="<?php echo htmlspecialchars($producto['ID_Producto']) ?>">
+              <a href="#" onclick="document.getElementById('<?php echo $formId; ?>').submit();">
+                <div class="cardLista">
+                  <div class="cardProdInicio">
+                    <img class="card-img-topProd img-square" src="./imgProductos/<?php echo htmlspecialchars($producto['Imagen_Producto']) ?>" alt="">
+                    <div class="card-bodyProd">
+                      <h5 class="card-titleProd"><?php echo htmlspecialchars($producto['Nombre_Producto']) ?></h5>
+                      <p class="text-infoProd"><?php echo "$ " . htmlspecialchars($producto['Precio_Producto']) ?></p>
+                      <?php if ($producto['Tiene_Stock_Producto'] == 0) { ?>
+                        <p class="text-danger stock-labelProd"><?php echo "Sin Stock" ?></p>
+                      <?php } ?>
+                    </div>
                   </div>
                 </div>
-              </div>
 
-            </a>
-          </form>
-        </div>
-      <?php } ?>
+              </a>
+            </form>
+          </div>
+        <?php } ?>
+      </div>
     </div>
-  </div>
+  <?php } ?>
 
-  <br>
-  <h3>Fútbol</h3>
-  <hr>
-  <?php
-  $categoriaSeleccionada = 'Fútbol'; 
-
-  $sentenciaSQL = $conexion->prepare("SELECT * FROM Productos INNER JOIN Categorias ON Productos.Categorias_ID_Categoria=Categorias.ID_Categoria WHERE Categorias.Nombre_Categoria=:categoria LIMIT 10");
-  $sentenciaSQL->bindParam(":categoria", $categoriaSeleccionada);
-  $sentenciaSQL->execute();
-  $listaProductos = $sentenciaSQL->fetchAll(PDO::FETCH_ASSOC);
-  ?>
-  <div class="">
-    <div class="row d-flex">
-      <?php foreach ($listaProductos as $producto) {
-        $formId = 'postForm' . htmlspecialchars($producto['ID_Producto']);
-      ?>
-        <div class="">
-          <form id="<?php echo $formId; ?>" action="productoDetalle.php" method="GET">
-            <input type="hidden" name="IdProducto" value="<?php echo htmlspecialchars($producto['ID_Producto']) ?>">
-            <a href="#" onclick="document.getElementById('<?php echo $formId; ?>').submit();">
-              <div class="cardLista">
-                <div class="cardProdInicio">
-                  <img class="card-img-topProd img-square" src="./imgProductos/<?php echo htmlspecialchars($producto['Imagen_Producto']) ?>" alt="">
-                  <div class="card-bodyProd">
-                    <h5 class="card-titleProd"><?php echo htmlspecialchars($producto['Nombre_Producto']) ?></h5>
-                    <p class="text-infoProd"><?php echo "$ " . htmlspecialchars($producto['Precio_Producto']) ?></p>
-                    <?php if ($producto['Tiene_Stock_Producto'] == 0) { ?>
-                      <p class="text-danger stock-labelProd"><?php echo "Sin Stock" ?></p>
-                    <?php } ?>
-                  </div>
-                </div>
-              </div>
-
-            </a>
-          </form>
-        </div>
-      <?php } ?>
-    </div>
-  </div>
-
-  <br>
-  <h3>Caza y Pesca</h3>
-  <hr>
-
-  <?php
-  $categoriaSeleccionada = 'Caza y Pesca'; 
-
-  $sentenciaSQL = $conexion->prepare("SELECT * FROM Productos INNER JOIN Categorias ON Productos.Categorias_ID_Categoria=Categorias.ID_Categoria WHERE Categorias.Nombre_Categoria=:categoria LIMIT 10");
-  $sentenciaSQL->bindParam(":categoria", $categoriaSeleccionada);
-  $sentenciaSQL->execute();
-  $listaProductos = $sentenciaSQL->fetchAll(PDO::FETCH_ASSOC);
-  ?>
-  <div class="">
-    <div class="row d-flex">
-      <?php foreach ($listaProductos as $producto) {
-        $formId = 'postForm' . htmlspecialchars($producto['ID_Producto']);
-      ?>
-        <div class="">
-          <form id="<?php echo $formId; ?>" action="productoDetalle.php" method="GET">
-            <input type="hidden" name="IdProducto" value="<?php echo htmlspecialchars($producto['ID_Producto']) ?>">
-            <a href="#" onclick="document.getElementById('<?php echo $formId; ?>').submit();">
-              <div class="cardLista">
-                <div class="cardProdInicio">
-                  <img class="card-img-topProd img-square" src="./imgProductos/<?php echo htmlspecialchars($producto['Imagen_Producto']) ?>" alt="">
-                  <div class="card-bodyProd">
-                    <h5 class="card-titleProd"><?php echo htmlspecialchars($producto['Nombre_Producto']) ?></h5>
-                    <p class="text-infoProd"><?php echo "$ " . htmlspecialchars($producto['Precio_Producto']) ?></p>
-                    <?php if ($producto['Tiene_Stock_Producto'] == 0) { ?>
-                      <p class="text-danger stock-labelProd"><?php echo "Sin Stock" ?></p>
-                    <?php } ?>
-                  </div>
-                </div>
-              </div>
-
-            </a>
-          </form>
-        </div>
-      <?php } ?>
-    </div>
-  </div>
-
-  <br>
-  <h3>Rugby</h3>
-  <hr>
-
-  <?php
-  $categoriaSeleccionada = 'Rugby'; 
-
-  $sentenciaSQL = $conexion->prepare("SELECT * FROM Productos INNER JOIN Categorias ON Productos.Categorias_ID_Categoria=Categorias.ID_Categoria WHERE Categorias.Nombre_Categoria=:categoria LIMIT 10");
-  $sentenciaSQL->bindParam(":categoria", $categoriaSeleccionada);
-  $sentenciaSQL->execute();
-  $listaProductos = $sentenciaSQL->fetchAll(PDO::FETCH_ASSOC);
-  ?>
-  <div class="">
-    <div class="row d-flex">
-      <?php foreach ($listaProductos as $producto) {
-        $formId = 'postForm' . htmlspecialchars($producto['ID_Producto']);
-      ?>
-        <div class="">
-          <form id="<?php echo $formId; ?>" action="productoDetalle.php" method="GET">
-            <input type="hidden" name="IdProducto" value="<?php echo htmlspecialchars($producto['ID_Producto']) ?>">
-            <a href="#" onclick="document.getElementById('<?php echo $formId; ?>').submit();">
-              <div class="cardLista">
-                <div class="cardProdInicio">
-                  <img class="card-img-topProd img-square" src="./imgProductos/<?php echo htmlspecialchars($producto['Imagen_Producto']) ?>" alt="">
-                  <div class="card-bodyProd">
-                    <h5 class="card-titleProd"><?php echo htmlspecialchars($producto['Nombre_Producto']) ?></h5>
-                    <p class="text-infoProd"><?php echo "$ " . htmlspecialchars($producto['Precio_Producto']) ?></p>
-                    <?php if ($producto['Tiene_Stock_Producto'] == 0) { ?>
-                      <p class="text-danger stock-labelProd"><?php echo "Sin Stock" ?></p>
-                    <?php } ?>
-                  </div>
-                </div>
-              </div>
-
-            </a>
-          </form>
-        </div>
-      <?php } ?>
-    </div>
-  </div>
-
-<?php include("template/pie.php") ?>
+  <?php include("template/pie.php"); ?>
