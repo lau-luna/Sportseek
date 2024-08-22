b734169134cebdabfccd028b50f064160800b43f
lautaro-luna <128639299+lautaro-luna@users.noreply.github.com>
2024-08-22 10:55:23 -0300

    Update cabecera.php

--- template/cabecera.php
+++ template/cabecera.php
@@ -19,7 +19,7 @@ ini_set('max_execution_time', '60');
 <html lang="es">
 
 <head>
-    <title>Sportsis</title>
+    <title>Sportseek</title>
 
     <!-- Required meta tags -->
     <meta charset="utf-8">
