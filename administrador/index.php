e454e8c95fdc7e5f29cb460e7df086ed4c935945
lautaro-luna <128639299+lautaro-luna@users.noreply.github.com>
2024-09-03 20:11:35 -0300

    manual de usuario

--- administrador/index.php
+++ administrador/index.php
@@ -5,9 +5,9 @@
                     <h1 class="display-3">Bienvenido, <?php echo $_SESSION['nombreUsuario'] ?></h1>
                     <p class="lead">Este es el apartado de administración del sitio web.</p>
                     <hr class="my-2">
-                    <p>More info</p>
+                    <p>Ayuda en el manual de usuario</p>
                     <p class="lead">
-                        <a class="btn btn-primary btn-lg" href="seccion/productos.php" role="button">Administrar productos</a>
+                        <a class="btn btn-primary btn-lg" href="https://docs.google.com/document/d/1JBjGBq4Kms9aHjZihXW_bNV5beE2SI_75tImMxC1bb8/edit?usp=sharing" role="button">Manual de usuario</a>
                     </p>
                 </div>
             </div>
