8b51fb518364ca14e027b4a468116f7c8c2e3d11
lautaro-luna <128639299+lau-luna@users.noreply.github.com>
2024-09-09 22:29:03 -0300

    agregar botón ayuda en contacto.php

--- contacto.php
+++ contacto.php
@@ -3,13 +3,18 @@
 <div class="jumbotron">
     <h1 class="display-3">Nosotros</h1>
     <p class="lead">Olimpiadas de informatica - I.P.E.T. y M. N° 257 "Dr. René Favaloro" - Laboulaye</p>
-    
+
     <ul>
         <li>Luna, Lautaro Eduardo Eber</li>
         <li>Wiesek, Lucio</li>
         <li>Gómez Molina, Lucio</li>
         <li>Loza, Enzo Jeremías</li>
     </ul>
+
+    <p>Ayuda en el manual de usuario</p>
+    <p class="lead">
+        <a class="btn btn-primary btn-lg" href="https://docs.google.com/document/d/1JBjGBq4Kms9aHjZihXW_bNV5beE2SI_75tImMxC1bb8/edit?usp=sharing" role="button">Manual de usuario</a>
+    </p>
 
     <hr class="my-2">
 </div>
