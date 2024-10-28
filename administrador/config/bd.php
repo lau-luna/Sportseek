31d7725e6c24a2bfd293bd29bd93a8502f13edd0
lautaro-luna <128639299+lau-luna@users.noreply.github.com>
2024-10-28 18:52:00 -0300

    Arreglo base de datos 2

--- administrador/config/bd.php
+++ administrador/config/bd.php
@@ -1,16 +1,15 @@
 <?php 
 try {
-    $host = getenv('DB_HOST');
-    $port = getenv('DB_PORT');
-    $bd = getenv('DB_NAME');
-    $usuario = getenv('DB_USER');
-    $contrasenia = getenv('DB_PASSWORD');
+    $host="lautaro-luna-lautaroluna906-b4ca.h.aivencloud.com";
+    $port = '17205';
+    $bd="sportseek";
+    $usuario="avnadmin";
+    $contrasenia="AVNS_aOVthXr6XdGle8n0350";
 
-    // Conexión PDO con la base de datos
-    $conexion = new PDO("mysql:host=$host;port=$port;dbname=$bd", $usuario, $contrasenia);
-    $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // Conexion PDO con la base de datos
+    $conexion=new PDO("mysql:host=$host;port=$port;dbname=$bd",$usuario,$contrasenia);
 
-} catch (PDOException $ex) {
-    // Manejo de errores más específico
-    echo "Error de conexión: " . $ex->getMessage();
+} catch ( Exception $ex) {
+    // En caso de fallo en la conexion con la BD
+    echo $ex->getMessage();
 }
