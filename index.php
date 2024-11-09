591da46f1c847790944ae4fae599ce5327293fa3
Lautaro Luna <lautaroluna906@gmail.com>
2024-11-09 01:26:50 -0300

    Test deploy SHH Raspberry Pi de Lau 2

--- index.php
+++ index.php
@@ -6,16 +6,16 @@
       <img src="./img/carrusel5.png" class="d-block w-100" style=" object-fit:cover;" alt="...">
     </div>
     <div class="carousel-item">
+      <img src="./img/carrusel1.png" class="d-block w-100" style=" object-fit:cover;" alt="...">
+    </div>
+    <div class="carousel-item">
       <img src="./img/carrusel2.png" class="d-block w-100" style=" object-fit:cover;" alt="...">
     </div>
     <div class="carousel-item">
-      <img src="./img/carrusel3.png" class="d-block w-100" style=" object-fit:cover;" alt="...">
+      <img src="./img/carrusel3.png" class="d-block w-100" style=" object-fit:contain;" alt="...">
     </div>
     <div class="carousel-item">
       <img src="./img/carrusel4.png" class="d-block w-100" style=" object-fit:contain;" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="./img/carrusel1.png" class="d-block w-100" style=" object-fit:contain;" alt="...">
     </div>
   </div>
 </div>
