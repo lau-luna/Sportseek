e0d2076da6c3fae915b22153dfced9ccaf0c4f6b
Lautaro Luna <lautaroluna906@gmail.com>
2024-11-12 09:26:26 -0300

    No sé

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php include("template/cabecera.php"); ?>
 
-<div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel" >
+<div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
   <div class="carousel-inner">
     <div class="carousel-item active">
       <img src="./formatoAvif/imgCarpeta/carrusel5.avif" class="d-block w-100" style=" object-fit:cover;" alt="...">
@@ -39,26 +39,26 @@
         $formId = 'postForm' . htmlspecialchars($producto['ID_Producto']);
       ?>
         <div class="mb-4 col-md-3">
-            <form id="<?php echo $formId; ?>" action="productoDetalle.php" method="GET">
-              <input type="hidden" name="IdProducto" value="<?php echo htmlspecialchars($producto['ID_Producto']) ?>">
-              <input type="hidden" name="txtCategoria" value="<?php echo htmlspecialchars($categoriaSeleccionada); ?>">
-              <input type="hidden" name="txtFiltro" value="<?php echo htmlspecialchars($filtroSeleccionado); ?>">
-              <a href="#" style="text-decoration: none;" onclick="document.getElementById('<?php echo $formId; ?>').submit();">
-                <div class="cardLista">
-                  <div class="cardProd" >
-                    <img class="card-img-topProd w" src="./comprimidas/<?php echo htmlspecialchars($producto['Imagen_Producto']) ?>" alt="">
-                    <div class="card-bodyProd">
-                      <h5 class="card-titleProd"><?php echo htmlspecialchars($producto['Nombre_Producto']) ?></h5>
-                      <p class="text-infoProd text-success"><?php echo "$ " . htmlspecialchars($producto['Precio_Producto']) ?></p>
-                      <?php if ($producto['Tiene_Stock_Producto'] == 0) { ?>
-                        <p class="text-danger stock-labelProd"><?php echo "Sin Stock" ?></p>
-                      <?php } ?>
-                    </div>
+          <form id="<?php echo $formId; ?>" action="productoDetalle.php" method="GET">
+            <input type="hidden" name="IdProducto" value="<?php echo htmlspecialchars($producto['ID_Producto']) ?>">
+            <input type="hidden" name="txtCategoria" value="<?php echo htmlspecialchars($categoriaSeleccionada); ?>">
+            <input type="hidden" name="txtFiltro" value="<?php echo htmlspecialchars($filtroSeleccionado); ?>">
+            <a href="#" style="text-decoration: none;" onclick="document.getElementById('<?php echo $formId; ?>').submit();">
+              <div class="cardLista">
+                <div class="cardProd">
+                  <img class="card-img-topProd w" src="./comprimidas/<?php echo htmlspecialchars($producto['Imagen_Producto']) ?>" alt="">
+                  <div class="card-bodyProd">
+                    <h5 class="card-titleProd"><?php echo htmlspecialchars($producto['Nombre_Producto']) ?></h5>
+                    <p class="text-infoProd text-success"><?php echo "$ " . htmlspecialchars($producto['Precio_Producto']) ?></p>
+                    <?php if ($producto['Tiene_Stock_Producto'] == 0) { ?>
+                      <p class="text-danger stock-labelProd"><?php echo "Sin Stock" ?></p>
+                    <?php } ?>
                   </div>
                 </div>
-              </a>
-            </form>
-          </div>
+              </div>
+            </a>
+          </form>
+        </div>
       <?php } ?>
     </div>
   </div>
@@ -82,15 +82,15 @@
           // Generar un ID único para cada formulario
           $formId = 'postForm' . htmlspecialchars($producto['ID_Producto']);
         ?>
-          <div class=" col-md-3" >
+          <div class="mb-4 col-md-3">
             <form id="<?php echo $formId; ?>" action="productoDetalle.php" method="GET">
               <input type="hidden" name="IdProducto" value="<?php echo htmlspecialchars($producto['ID_Producto']) ?>">
               <input type="hidden" name="txtCategoria" value="<?php echo htmlspecialchars($categoriaSeleccionada); ?>">
               <input type="hidden" name="txtFiltro" value="<?php echo htmlspecialchars($filtroSeleccionado); ?>">
               <a href="#" style="text-decoration: none;" onclick="document.getElementById('<?php echo $formId; ?>').submit();">
                 <div class="cardLista">
-                  <div class="cardProd" style="width: 100%;">
-                    <img class="card-img-topProd img-square" src="./comprimidas/<?php echo htmlspecialchars($producto['Imagen_Producto']) ?>" alt="">
+                  <div class="cardProd">
+                    <img class="card-img-topProd w" src="./comprimidas/<?php echo htmlspecialchars($producto['Imagen_Producto']) ?>" alt="">
                     <div class="card-bodyProd">
                       <h5 class="card-titleProd"><?php echo htmlspecialchars($producto['Nombre_Producto']) ?></h5>
                       <p class="text-infoProd text-success"><?php echo "$ " . htmlspecialchars($producto['Precio_Producto']) ?></p>
