71244756142b34c349717aa0da4a50982ab91df3
Enzo Loza <folomlojo@gmail.com>
2024-11-12 23:40:10 -0300

    no se puede pedir cosas menores a 1
    tracate

--- productoDetalle.php
+++ productoDetalle.php
@@ -5,19 +5,16 @@
 <br>
 <?php
 if (preg_match('/^[0-9]+$/', $_GET['IdProducto'])) {
-    // Obtener el producto específico
     $sentenciaSQL = $conexion->prepare("SELECT * FROM Productos WHERE ID_Producto = :id");
     $sentenciaSQL->bindParam(':id', $_GET['IdProducto']);
     $sentenciaSQL->execute();
     $producto = $sentenciaSQL->fetch(PDO::FETCH_ASSOC);
 
-    // Obtener la categoría del producto
     $sentenciaSQL = $conexion->prepare("SELECT Categorias.Nombre_Categoria FROM Productos INNER JOIN Categorias ON Categorias.ID_Categoria = Productos.Categorias_ID_Categoria WHERE Productos.ID_Producto = :id");
     $sentenciaSQL->bindParam(':id', $_GET['IdProducto']);
     $sentenciaSQL->execute();
     $categoria = $sentenciaSQL->fetch(PDO::FETCH_ASSOC);
 
-    // Formatear el precio del producto
     $precioFormateado = number_format($producto['Precio_Producto'], 0, ',', '.');
 }
 ?>
@@ -50,11 +47,9 @@ if (preg_match('/^[0-9]+$/', $_GET['IdProducto'])) {
                             <input type="hidden" name="IdProducto" value="<?php echo htmlspecialchars($producto['ID_Producto']) ?>">
                             <label for="cantidad">Cantidad:</label>
                             <input type="number" min="1" value="1" id="cantidad" class="form-control col-md-2 mb-2" name="cantidadProducto" required oninput="this.value = this.value.replace(/[^0-9]/g, '')">
-
-                            <a href="#" onclick="document.getElementById('form').submit();">
-                                <button type="button" class="btn btn-success mb-4">Agregar al carrito</button>
-                            </a>
+                            <button type="submit" class="btn btn-success mb-4">Agregar al carrito</button>
                         </form>
+
                     <?php } ?>
 
                     <p class="mb-0" style="font-size: large; font-weight: bold;">Descripción</p>
@@ -77,7 +72,7 @@ if (preg_match('/^[0-9]+$/', $_GET['IdProducto'])) {
 function validateForm() {
     var cantidad = document.getElementById("cantidad").value;
     if (isNaN(cantidad) || cantidad <= 0) {
-        alert("Por favor, ingrese una cantidad válida.");
+        alert("Por favor, ingrese una cantidad válida mayor a 0.");
         return false; // Evita que el formulario se envíe si la cantidad no es válida
     }
     return true; // Permite que el formulario se envíe si la cantidad es válida
@@ -85,9 +80,6 @@ function validateForm() {
 </script>
 
 <?php include("template/pie.php"); ?>
-
-<?php include("template/cabecera.php"); ?>
-
 <br><br>
 
 <?php
@@ -98,18 +90,14 @@ if (!isset($_SESSION['ID_Usuario'])) {
     echo "<script>window.location.href='loginUsuario.php';</script>";
 }
 
-// Obtener carrito del usuario
 $sentenciaSQL = $conexion->prepare("SELECT * FROM Carritos INNER JOIN Usuarios ON Usuarios.ID_Usuario = Carritos.Usuarios_ID_Usuario WHERE Usuarios.ID_Usuario = :IdUsuario");
 $sentenciaSQL->bindParam(":IdUsuario", $_SESSION['ID_Usuario']);
 $sentenciaSQL->execute();
 $carrito = $sentenciaSQL->fetch(PDO::FETCH_ASSOC);
 
-// Si el usuario tiene un carrito guardado
 if (isset($carrito['ID_Carrito'])) {
 
-    // Si se manda un producto desde productoDetalle.php
     if (isset($_POST['IdProducto']) && preg_match('/^[0-9]+$/', $_POST['IdProducto'])) {
-        // Comprobar si ya tiene el producto seleccionado para aumentar su cantidad
         $sentenciaSQL = $conexion->prepare("SELECT Productos_ID_Producto, Cantidad_Productos FROM Carritos_Productos WHERE Carritos_ID_Carrito = :IdCarrito AND Productos_ID_Producto = :IdProducto");
         $sentenciaSQL->bindParam(":IdCarrito", $carrito['ID_Carrito']);
         $sentenciaSQL->bindParam(":IdProducto", $_POST['IdProducto']);
@@ -124,7 +112,6 @@ if (isset($carrito['ID_Carrito'])) {
             $sentenciaSQL->bindParam(":cantidad", $cantidad);
             $sentenciaSQL->execute();
         } else {
-            // Insertar el nuevo producto seleccionado en el carrito
             $sentenciaSQL = $conexion->prepare("INSERT INTO Carritos_Productos (Carritos_ID_Carrito, Productos_ID_Producto, Cantidad_Productos) VALUES (:IdCarrito, :IdProducto, :cantidad)");
             $sentenciaSQL->bindParam(":IdCarrito", $carrito['ID_Carrito']);
             $sentenciaSQL->bindParam(":IdProducto", $_POST['IdProducto']);
@@ -133,7 +120,6 @@ if (isset($carrito['ID_Carrito'])) {
         }
     }
 } else {
-    // Crear nuevo carrito si no existe
     $fecha = new DateTime();
     $fechaCarrito = date('Y-m-d H:i:s', $fecha->getTimestamp());
 
@@ -142,11 +128,9 @@ if (isset($carrito['ID_Carrito'])) {
     $sentenciaSQL->bindParam(":fecha", $fechaCarrito);
     $sentenciaSQL->execute();
 
-    // Obtener el ID del carrito recién creado
     $carrito['ID_Carrito'] = $conexion->lastInsertId();
 
     if (isset($_POST['IdProducto']) && preg_match('/^[0-9]+$/', $_POST['IdProducto'])) {
-        // Insertar productos en el nuevo carrito
         $sentenciaSQL = $conexion->prepare("INSERT INTO Carritos_Productos (Carritos_ID_Carrito, Productos_ID_Producto, Cantidad_Productos) VALUES (:IdCarrito, :IdProducto, :cantidad)");
         $sentenciaSQL->bindParam(":IdCarrito", $carrito['ID_Carrito']);
         $sentenciaSQL->bindParam(":IdProducto", $_POST['IdProducto']);
@@ -155,9 +139,9 @@ if (isset($carrito['ID_Carrito'])) {
     }
 }
 
-// Obtener lista de productos en el carrito
 $sentenciaSQL = $conexion->prepare("SELECT Productos.*, Carritos_Productos.Cantidad_Productos FROM Carritos_Productos INNER JOIN Productos ON Carritos_Productos.Productos_ID_Producto = Productos.ID_Producto WHERE Carritos_Productos.Carritos_ID_Carrito = :IdCarrito");
 $sentenciaSQL->bindParam(":IdCarrito", $carrito['ID_Carrito']);
 $sentenciaSQL->execute();
 $listaCarritosProductos = $sentenciaSQL->fetchAll(PDO::FETCH_ASSOC);
 ?>
+
