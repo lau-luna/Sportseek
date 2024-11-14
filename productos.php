0eead6ffa6237c74266e0c8d56aed4350dd04c70
Lautaro Luna <lautaroluna906@gmail.com>
2024-11-13 23:10:45 -0300

    dou

--- productos.php
+++ productos.php
@@ -14,7 +14,6 @@ $params = [];
 // Crear las condiciones para nombre o categoría
 if ($busqueda) {
     foreach ($palabras as $index => $palabra) {
-        // Añadir las condiciones para cada palabra en la búsqueda
         $params[":busqueda$index"] = "%" . strtolower($palabra) . "%";
         $condiciones[] = "(LOWER(Productos.Nombre_Producto) LIKE :busqueda$index OR LOWER(Categorias.Nombre_Categoria) LIKE :busqueda$index)";
     }
@@ -30,14 +29,10 @@ $offset = ($paginaActual - 1) * $productosPorPagina;
 
 // Obtener la categoría seleccionada y el filtro desde GET
 $categoriaSeleccionada = (isset($_GET['txtCategoria'])) ? $_GET['txtCategoria'] : 'todas';
-$filtroSeleccionado = (isset($_GET['txtFiltro']) && preg_match('/^[a-zA-Z]+$/',  $_GET['txtFiltro'])) ? $_GET['txtFiltro'] : 'ninguno';
+$filtroSeleccionado = (isset($_GET['txtFiltro']) && preg_match('/^[a-zA-Z]+$/', $_GET['txtFiltro'])) ? $_GET['txtFiltro'] : 'ninguno';
 
 // Preparar la consulta SQL base
-$sqlBase = "SELECT Productos.*, Categorias.Nombre_Categoria, 
-            MATCH (Productos.Nombre_Producto) AGAINST (:busqueda IN BOOLEAN MODE) AS relevance
-            FROM Productos
-            INNER JOIN Categorias ON Productos.Categorias_ID_Categoria = Categorias.ID_Categoria
-            WHERE $condicionesStr";
+$sqlBase = "SELECT Productos.*, Categorias.Nombre_Categoria, MATCH (Productos.Nombre_Producto) AGAINST (:busqueda IN BOOLEAN MODE) AS relevance FROM Productos INNER JOIN Categorias ON Productos.Categorias_ID_Categoria = Categorias.ID_Categoria WHERE $condicionesStr";
 
 // Añadir filtro de categoría si es necesario
 if ($categoriaSeleccionada != 'todas') {
@@ -45,14 +40,7 @@ if ($categoriaSeleccionada != 'todas') {
 }
 
 // Añadir filtro y ordenamiento
-$sqlBase .= " ORDER BY 
-            CASE 
-                WHEN :filtro = 'precioMasBajo' THEN Precio_Producto 
-                WHEN :filtro = 'precioMasAlto' THEN Precio_Producto * -1 
-                WHEN :busqueda = '' THEN ID_Producto
-                ELSE relevance 
-            END DESC 
-            LIMIT :limit OFFSET :offset";
+$sqlBase .= " ORDER BY CASE WHEN :filtro = 'precioMasBajo' THEN Precio_Producto WHEN :filtro = 'precioMasAlto' THEN Precio_Producto * -1 WHEN :busqueda = '' THEN ID_Producto ELSE relevance END DESC LIMIT :limit OFFSET :offset";
 
 // Preparar la consulta
 $sentenciaSQL = $conexion->prepare($sqlBase);
@@ -61,6 +49,7 @@ $sentenciaSQL = $conexion->prepare($sqlBase);
 foreach ($params as $param => $value) {
     $sentenciaSQL->bindValue($param, $value, PDO::PARAM_STR);
 }
+
 if ($categoriaSeleccionada != 'todas') {
     $sentenciaSQL->bindValue(':IdCategoria', $categoriaSeleccionada, PDO::PARAM_INT);
 }
@@ -70,16 +59,19 @@ $sentenciaSQL->bindValue(':offset', $offset, PDO::PARAM_INT);
 $sentenciaSQL->bindValue(':busqueda', $busqueda, PDO::PARAM_STR);
 
 // Ejecutar la consulta
-$sentenciaSQL->execute();
-$listaProductos = $sentenciaSQL->fetchAll(PDO::FETCH_ASSOC);
+try {
+    $sentenciaSQL->execute();
+    $listaProductos = $sentenciaSQL->fetchAll(PDO::FETCH_ASSOC);
+} catch (PDOException $e) {
+    echo "Error en la consulta: " . htmlspecialchars($e->getMessage());
+}
 
 // Obtener el número total de productos que coinciden con la búsqueda
-$sqlCount = "SELECT COUNT(*) FROM Productos
-            INNER JOIN Categorias ON Productos.Categorias_ID_Categoria = Categorias.ID_Categoria
-            WHERE $condicionesStr";
+$sqlCount = "SELECT COUNT(*) FROM Productos INNER JOIN Categorias ON Productos.Categorias_ID_Categoria = Categorias.ID_Categoria WHERE $condicionesStr";
 if ($categoriaSeleccionada != 'todas') {
     $sqlCount .= " AND Productos.Categorias_ID_Categoria = :IdCategoria";
 }
+
 $sentenciaSQLCount = $conexion->prepare($sqlCount);
 if ($categoriaSeleccionada != 'todas') {
     $sentenciaSQLCount->bindValue(":IdCategoria", $categoriaSeleccionada, PDO::PARAM_INT);
@@ -87,22 +79,19 @@ if ($categoriaSeleccionada != 'todas') {
 foreach ($params as $param => $value) {
     $sentenciaSQLCount->bindValue($param, $value, PDO::PARAM_STR);
 }
+
 $sentenciaSQLCount->execute();
 $totalProductos = $sentenciaSQLCount->fetchColumn();
 $totalPaginas = ceil($totalProductos / $productosPorPagina);
 
 // Verificar si la página solicitada es mayor que el total de páginas disponibles
-if ($paginaActual > $totalPaginas) {
-    // Redirige a la primera página si la página solicitada es mayor que el total de páginas
+if ($paginaActual > $totalPaginas && $totalPaginas > 0) {
     header("Location: ?busqueda=" . urlencode($busqueda) . "&txtCategoria=" . urlencode($categoriaSeleccionada) . "&txtFiltro=" . urlencode($filtroSeleccionado) . "&pagina=1");
     exit;
 }
 ?>
 
-<br>
-<br>
-<br>
-
+<br><br><br>
 <div class="contenedorProductos" id="filtros">
     <!-- Sidebar -->
     <aside class="ml-4 mr-3 aside-desktop">
@@ -121,22 +110,18 @@ if ($paginaActual > $totalPaginas) {
                 <input type="hidden" name="pagina" value="1"> <!-- Reinicia la página a la 1 al cambiar de categoría -->
             </div>
         </form>
-
         <div class="card">
-            <div class="card-header">
-                Categorías
-            </div>
+            <div class="card-header">Categorías</div>
             <div>
                 <form method="GET">
                     <!-- Botón para Todas las categorías -->
-                    <button type="submit" class="btn text-secondary" style="width: 100%; text-align:left; <?php if ($categoriaSeleccionada == 'todas') echo 'font-weight: bold;'; ?>" name="txtCategoria" value="todas">
-                        Todas las categorías
-                    </button>
-
-                    <?php
+                    <button type="submit" class="btn text-secondary" style="width: 100%; text-align:left; <?php if ($categoriaSeleccionada == 'todas') echo 'font-weight: bold;'; ?>" name="txtCategoria" value="todas">Todas las categorías</button>
+                    <?php 
+                    // Obtener categorías desde la base de datos
                     $sentenciaSQL = $conexion->prepare("SELECT * FROM Categorias");
                     $sentenciaSQL->execute();
                     $listaCategorias = $sentenciaSQL->fetchAll(PDO::FETCH_ASSOC);
+
                     foreach ($listaCategorias as $categoria) { ?>
                         <button style="width: 100%; text-align:left; <?php if ($categoriaSeleccionada == $categoria['ID_Categoria']) echo 'font-weight: bold;'; ?>" type="submit" class="btn text-secondary" name="txtCategoria" value="<?php echo htmlspecialchars($categoria['ID_Categoria']); ?>">
                             <?php echo htmlspecialchars($categoria['Nombre_Categoria']); ?>
@@ -150,76 +135,13 @@ if ($paginaActual > $totalPaginas) {
         </div>
     </aside>
 
-    <aside class="ml-4 mr-3 aside-mobile">
-        <!-- Formulario de filtros -->
-        <div class="filtros">
-            <form method="GET" action="">
-                <div data-mdb-input-init class="mb-2">
-                    <label class="form-label">Filtrar por:</label>
-                    <select name="txtFiltro" id="filtro" class="form-control" onchange="this.form.submit()">
-                        <option value="ninguno" <?php if ($filtroSeleccionado == 'ninguno') echo 'selected'; ?>>Sin filtro</option>
-                        <option value="precioMasBajo" <?php if ($filtroSeleccionado == 'precioMasBajo') echo 'selected'; ?>>Precio más alto</option>
-                        <option value="precioMasAlto" <?php if ($filtroSeleccionado == 'precioMasAlto') echo 'selected'; ?>>Precio más bajo</option>
-                    </select>
-                    <!-- Campo oculto para mantener la búsqueda -->
-                    <input type="hidden" name="busqueda" value="<?php echo htmlspecialchars($busqueda); ?>">
-                    <input type="hidden" name="txtCategoria" value="<?php echo htmlspecialchars($categoriaSeleccionada); ?>">
-                    <input type="hidden" name="pagina" value="<?php echo htmlspecialchars($paginaActual); ?>">
-                </div>
-            </form>
-        </div>
-
-        <div class="categorias">
-            <form method="GET">
-                <div class="dropdown dropdownCategorias">
-                    <a class="btn btn-outline-secondary dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Categorías
-                    </a>
-
-                    <ul class="dropdown-menu">
-                        <li>
-                            <button type="submit" class="btn text-secondary" style="width: 100%; text-align:left; <?php if ($categoriaSeleccionada == 'todas') echo 'font-weight: bold;'; ?>" name="txtCategoria" value="todas">
-                                Todas las categorías
-                            </button>
-                        </li>
-
-                        <?php
-
-                        $sentenciaSQL = $conexion->prepare("SELECT * FROM Categorias");
-                        $sentenciaSQL->execute();
-                        $listaCategorias = $sentenciaSQL->fetchAll(PDO::FETCH_ASSOC);
-                        foreach ($listaCategorias as $categoria) { ?>
-                            <li>
-                                <button style="width: 100%; text-align:left; <?php if ($categoriaSeleccionada == $categoria['ID_Categoria']) echo 'font-weight: bold;'; ?>" type="submit" class="btn text-secondary" name="txtCategoria" value="<?php echo htmlspecialchars($categoria['ID_Categoria']); ?>">
-                                    <?php echo htmlspecialchars($categoria['Nombre_Categoria']); ?>
-                                </button>
-                            </li>
-
-                        <?php } ?>
-                        <!-- Mantener el filtro seleccionado en la URL -->
-                        <input type="hidden" name="txtFiltro" value="<?php echo htmlspecialchars($filtroSeleccionado); ?>">
-                        <input type="hidden" name="pagina" value="<?php if ($categoriaSeleccionada == 'todas' && $paginaActual != 1) {
-                                                                        echo htmlspecialchars(1);
-                                                                    } else {
-                                                                        echo htmlspecialchars($paginaActual);
-                                                                    } ?>">
-                    </ul>
-                </div>
-            </form>
-
-
-        </div>
-
-    </aside>
-
     <!-- Productos -->
     <div class="col-md-10">
         <div class="row">
             <?php if (!empty($listaProductos)) { ?>
-                <?php foreach ($listaProductos as $producto) {
-                    // Generar un ID único para cada formulario
-                    $formId = 'postForm' . htmlspecialchars($producto['ID_Producto']);
-                ?>
+                <?php foreach ($listaProductos as $producto) { 
+                    // Generar un ID único para cada formulario 
+                    $formId = 'postForm' . htmlspecialchars($producto['ID_Producto']); ?>
                     <div class="col-md-3">
                         <form id="<?php echo $formId; ?>" action="productoDetalle.php" method="GET">
                             <input type="hidden" name="IdProducto" value="<?php echo htmlspecialchars($producto['ID_Producto']) ?>">
@@ -233,7 +155,7 @@ if ($paginaActual > $totalPaginas) {
                                             <h5 class="card-titleProd-productos"><?php echo htmlspecialchars($producto['Nombre_Producto']) ?></h5>
                                             <p class="text-infoProd-productos text-success"><?php echo "$ " . htmlspecialchars($producto['Precio_Producto']) ?></p>
                                             <?php if ($producto['Tiene_Stock_Producto'] == 0) { ?>
-                                                <p class="text-danger stock-labelProd"><?php echo "Sin Stock" ?></p>
+                                                <p class="text-danger stock-labelProd"><?php echo "Sin Stock"; ?></p>
                                             <?php } ?>
                                         </div>
                                     </div>
@@ -243,9 +165,8 @@ if ($paginaActual > $totalPaginas) {
                     </div>
                 <?php } ?>
             <?php } else { ?>
-                <div class="alert alert-info" role="alert">
-                    No se encontraron productos que coincidan con la búsqueda.
-                </div>
+                <!-- Mensaje cuando no hay productos que coincidan con la búsqueda -->
+                <div class="alert alert-info" role="alert">No se encontraron productos que coincidan con la búsqueda.</div>
             <?php } ?>
         </div>
 
@@ -254,14 +175,13 @@ if ($paginaActual > $totalPaginas) {
             <ul class="pagination">
                 <?php for ($i = 1; $i <= $totalPaginas; $i++) { ?>
                     <li class="page-item <?php if ($paginaActual == $i) echo 'active'; ?>">
-                        <a class="page-link" href="?busqueda=<?php echo urlencode($busqueda); ?>&txtCategoria=<?php echo urlencode($categoriaSeleccionada); ?>&txtFiltro=<?php echo urlencode($filtroSeleccionado); ?>&pagina=<?php echo $i; ?>">
-                            <?php echo $i; ?>
-                        </a>
+                        <a class="page-link" href="?busqueda=<?php echo urlencode($busqueda); ?>&txtCategoria=<?php echo urlencode($categoriaSeleccionada); ?>&txtFiltro=<?php echo urlencode($filtroSeleccionado); ?>&pagina=<?php echo $i; ?>"><?php echo $i; ?></a>
                     </li>
                 <?php } ?>
             </ul>
         </div>
-    </div>
-</div>
+
+    </div> <!-- Fin de col-md-10 -->
+</div> <!-- Fin de contenedorProductos -->
 
 <?php include("template/pie.php"); ?>
